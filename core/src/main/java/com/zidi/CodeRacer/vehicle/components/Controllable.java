cbfca2daf3694aca3805bbae7c4c9c191a6af6b5
zidiyao <zidiyaocanada@outlook.com>
2025-10-20 23:34:22 -0400

    update

--- core/src/main/java/com/zidi/CodeRacer/vehicle/components/Controllable.java
+++ core/src/main/java/com/zidi/CodeRacer/vehicle/components/Controllable.java
@@ -1,4 +1,9 @@
 package com.zidi.CodeRacer.vehicle.components;
 
+/**
+ *
+ * 玩家到时候可以写程序单独去控制，比如 武器，弹跳等。
+ * 不能控制的是比如，油箱，Frame，甚至轮子是否应该算在可控制里需要思考一下
+ */
 public interface Controllable {
 }
--- core/src/main/java/com/zidi/CodeRacer/vehicle/components/engine/Engine.java
+++ core/src/main/java/com/zidi/CodeRacer/vehicle/components/engine/Engine.java
@@ -1,4 +1,20 @@
 package com.zidi.CodeRacer.vehicle.components.engine;
 
 public interface Engine {
+
+    int getCurrentSpeed();
+
+    int getMaxSpeed();
+
+    int getFuelConsumptionRate();
+
+    void accelerate(float deltaTime);
+
+    void decelerate(float deltaTime);
+
+
+
+
+
+
 }
